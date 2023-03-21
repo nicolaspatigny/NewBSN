c962b73a98ed47eed93dbcb9a969a3f9820f1264
patigny.nicolas <Niiyugitowaifu1>
2023-03-21 09:59:28 +0100

    Inscription 1-4

--- app/src/main/java/com/example/newbsn/Inscription2.kt
+++ app/src/main/java/com/example/newbsn/Inscription2.kt
@@ -1,20 +1,23 @@
 package com.example.newbsn
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class Inscription1 : AppCompatActivity() {
+class Inscription2 : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_inscription1)
-        val button = findViewById<Button>(R.id.button_email_inscription)
+        setContentView(R.layout.activity_inscription2)
+        val button = findViewById<Button>(R.id.telephone)
 
         button.setOnClickListener {
-            val intent = Intent(this, Inscription2::class.java)
+            val intent = Intent(this, Inscription1::class.java)
             startActivity(intent)
         }
+
         val button1 = findViewById<Button>(R.id.button_continuer_inscription_telepone)
 
         button1.setOnClickListener {
---
+++ app/src/main/java/com/example/newbsn/Inscription4.kt
@@ -0,0 +1,11 @@
+package com.example.newbsn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Inscription4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inscription4)
+    }
+}
